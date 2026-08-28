9d4733d2c526e374c9be8e45b6b00311881d4644
aidartheklutz <aid4r@proton.me>
2026-08-28 20:16:54 +0600

    update description

--- src/pages/HomePage/LangHomePage.jsx
+++ src/pages/HomePage/LangHomePage.jsx
@@ -6,7 +6,7 @@ export const LANG = {
     description: (
       <p className="blue-link">
         I am a school student from Kyrgyzstan and I aspire to become an
-        engineer. Here you can learn a little{" "}
+        electronics engineer. Here you can learn a little{" "}
         <NavLink to="/aboutme">about me</NavLink>, read my{" "}
         <NavLink to="/blog">blog</NavLink>, check out some of the{" "}
         <NavLink to="/projects">projects</NavLink> of mine, or find my{" "}
@@ -20,9 +20,9 @@ export const LANG = {
     greeting: "Привет, я Айдар!",
     description: (
       <p className="blue-link">
-        Я студент колледжа из Кыргызстана, который мечтает стать инженером.
-        Здесь вы можете узнать немного <NavLink to="/aboutme">обо мне</NavLink>,
-        читать мой <NavLink to="/blog">блог</NavLink>, взглянуть на мои{" "}
+        Я студент колледжа из Кыргызстана. Здесь вы можете узнать немного{" "}
+        <NavLink to="/aboutme">обо мне</NavLink>, читать мой{" "}
+        <NavLink to="/blog">блог</NavLink>, взглянуть на мои{" "}
         <NavLink to="/projects">проекты</NavLink> или найти мои{" "}
         <NavLink to="/links">соцсети</NavLink>.
       </p>
